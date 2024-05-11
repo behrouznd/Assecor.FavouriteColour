cf1002805ebecd734d1b937d259ad703f8753d30
behrouznd <behrouz1986@gmail.com>
2024-05-11 14:09:49 +0200

    Create Person Entity

---
+++ Domain/Entities/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Common;
+
+namespace Entities.Models;
+
+public class Person : BaseEntityInt
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Address { get; set; }
+    public int Color { get; set; }
+}
+
