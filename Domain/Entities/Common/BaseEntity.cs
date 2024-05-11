9bb44ec17fdc4c5670d33f65d5550f344a96364a
behrouznd <behrouz1986@gmail.com>
2024-05-11 14:09:35 +0200

    Create BaseEntity

---
+++ Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Common;
+
+public abstract class BaseEntity<TKey>
+{
+    [Key]
+    public TKey Id { get; set; }
+}
---
+++ Domain/Entities/Common/BaseEntityInt.cs
@@ -0,0 +1,5 @@
+﻿namespace Entities.Common;
+
+public class BaseEntityInt : BaseEntity<int>
+{
+}
