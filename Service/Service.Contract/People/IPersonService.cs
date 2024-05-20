4638e40b96000688dd9117ac3fc0f950716a69b8
behrouznd <behrouz1986@gmail.com>
2024-05-20 22:02:58 +0200

    Create PersonService interface

---
+++ Service/Service.Contract/People/IPersonService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DataTransferObject.People;
+
+namespace Service.Contract.People;
+
+public interface IPersonService
+{
+    IReadOnlyCollection<PersonDto> GetPeople();
+    PersonDto GetPersonById(int id);
+    IReadOnlyCollection<PersonDto> GetPeopleByColor(int id);
+    PersonDto AddPerson(PersonDto person);
+}
