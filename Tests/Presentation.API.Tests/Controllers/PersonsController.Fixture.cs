1f8deeb95868e8b7bffab027b62f8d3f0b15afc0
behrouznd <behrouz1986@gmail.com>
2024-05-23 00:11:33 +0200

    create UnitTest for Persons API

---
+++ Tests/Presentation.API.Tests/Controllers/PersonsController.Fixture.cs
@@ -0,0 +1,103 @@
+﻿using NSubstitute;
+using Service.Contract.People;
+using Shared.DataTransferObject.People;
+
+namespace Presentation.API.Tests.Controllers;
+
+public partial class PersonsController : IAsyncLifetime
+{
+    private TestFixture fixture { get; } = new();
+
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    private class TestFixture
+    {
+        public readonly Mock Mocks = new();
+        public readonly TestData Data = new();
+
+        public class Mock
+        {
+            public readonly IPersonService personService = Substitute.For<IPersonService>();
+
+            public void SetupGetPersons(IReadOnlyCollection<PersonDto> people)
+            {
+                personService
+                    .GetPeople()
+                    .Returns(people);
+            }
+
+            public void SetupGetPersonById(PersonDto person)
+            {
+                personService
+                    .GetPersonById(Arg.Any<int>())
+                    .Returns(person);
+            }
+
+            public void SetupGetPersonsByColor(IReadOnlyCollection<PersonDto> people)
+            {
+                personService
+                    .GetPeopleByColor(Arg.Any<int>())
+                    .Returns(people);
+            }
+
+            public void SetupAddPerson(PersonDto person)
+            {
+                personService
+                    .AddPerson(person)
+                    .Returns(person);
+            }
+        }
+
+        public class TestData
+        {
+            public IReadOnlyCollection<PersonDto> People => new List<PersonDto>()
+            {
+                new PersonDto()
+                {
+                    id = 1,
+                    name = "Pink",
+                    lastname = "Panther",
+                    zipcode = "12345",
+                    city = "london",
+                    color = "rot",
+                },
+                new PersonDto()
+                {
+                    id = 2,
+                    name = "Mickey",
+                    lastname = "Mouse",
+                    zipcode = "54321",
+                    city = "berlin",
+                    color = "blau",
+                },
+                new PersonDto()
+                {
+                    id = 3,
+                    name = "Donald",
+                    lastname = "Duck",
+                    zipcode = "456789",
+                    city = "bonn",
+                    color = "rot",
+                }
+            };
+
+            public PersonDto Person =>
+                new PersonDto()
+                {
+                    id = 1,
+                    name = "Pink",
+                    lastname = "Panther",
+                    zipcode = "12345",
+                    city = "london",
+                    color = "rot",
+                };
+        }
+    
+        public API.Controllers.PersonsController CreateSystemUnderTest()
+        {
+            return new API.Controllers.PersonsController(Mocks.personService);
+        }
+    }
+}
---
+++ Tests/Presentation.API.Tests/Controllers/PersonsController.cs
@@ -0,0 +1,179 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Shared.DataTransferObject.People;
+
+namespace Presentation.API.Tests.Controllers;
+
+public partial class PersonsController
+{
+    public class GetPersons : PersonsController
+    {
+        [Fact]
+        public void ShouldReturnPersons()
+        {
+            //Arrange
+            var expectedPersons = fixture.Data.People;
+            fixture.Mocks.SetupGetPersons(expectedPersons);
+            var sut = fixture.CreateSystemUnderTest();
+
+            //Act
+            var actionResult = sut.GetPersons();
+
+            //Assert
+            var result = actionResult.Should()
+                .NotBeNull()
+                .And.BeOfType<OkObjectResult>()
+                .Subject
+                ;
+
+            var actualPersonsResult = Assert.IsAssignableFrom<IReadOnlyCollection<PersonDto>>(result.Value);
+
+            actualPersonsResult.Should()
+                .NotBeNull()
+                .And.NotBeEmpty()
+                .And.HaveCount(expectedPersons.Count)
+                .And.BeEquivalentTo(expectedPersons)
+                ;
+        }
+    }
+
+    public class GetPerson : PersonsController
+    {
+        [Fact]
+        public void ShouldReturnPerson()
+        {
+            //Arrange
+            var personId = 1;
+            var expectedPerson = fixture.Data.Person;
+            fixture.Mocks.SetupGetPersonById(expectedPerson);
+            var sut = fixture.CreateSystemUnderTest();
+
+            //Act
+            var actionResult = sut.GetPerson(personId);
+
+            //Assert
+            var result = actionResult.Should()
+                .NotBeNull()
+                .And.BeOfType<OkObjectResult>()
+                .Subject
+                ;
+
+            var actualPersonResult = Assert.IsAssignableFrom<PersonDto>(result.Value);
+
+            actualPersonResult.Should()
+                .NotBeNull()
+                .And.BeEquivalentTo(expectedPerson)
+                ;
+        }
+    }
+
+    public class GetPersonsByColor : PersonsController 
+    {
+        [Theory]
+        [InlineData("rot")]
+        [InlineData("blau")]
+        public void ShouldReturnPerson_WhenDataWasFound(string color)
+        {
+            // Arrange
+            var expectedResult = fixture.Data.People.Where(x => x.color == color).ToList();
+            fixture.Mocks.SetupGetPersonsByColor(expectedResult);
+            var sut = fixture.CreateSystemUnderTest();
+
+            // Act
+            var  actionResult = sut.GetPersonsByColor(color);
+
+            // Assert
+            var result = actionResult.Should()
+                .NotBeNull()
+                .And.BeOfType<OkObjectResult>()
+                .Subject
+                ;
+
+            var actualPersonResult = Assert.IsAssignableFrom<IReadOnlyCollection<PersonDto>>(result.Value);
+            actualPersonResult.Should()
+                .NotBeNull()
+                .And.HaveCount(expectedResult.Count)
+                .And.BeEquivalentTo(expectedResult)
+                .And.AllSatisfy(actualPerson =>
+                {
+                    actualPerson.color.Should().Be(color);
+                })
+                ;
+        }
+
+        [Fact]
+        public void ShouldReturnPerson_WhenDataDoesNotExist()
+        {
+            // Arrange
+            string color = "gelb";
+            var expectedResult = fixture.Data.People.Where(x => x.color == color).ToList();
+            fixture.Mocks.SetupGetPersonsByColor(expectedResult);
+            var sut = fixture.CreateSystemUnderTest();
+
+            // Act
+            var actionResult = sut.GetPersonsByColor(color);
+
+            // Assert
+            var result = actionResult.Should()
+                .NotBeNull()
+                .And.BeOfType<OkObjectResult>()
+                .Subject
+                ;
+
+            var actualPersonResult = Assert.IsAssignableFrom<IReadOnlyCollection<PersonDto>>(result.Value);
+
+            actualPersonResult.Should()
+                .NotBeNull()
+                .And.BeEmpty()
+                ;
+        }
+
+
+        [Fact]
+        public void ShouldReturnBadRequest_WhenParamIsInvalid()
+        {
+            // Arrange
+            var expectedResult = fixture.Data.People;
+            fixture.Mocks.SetupGetPersonsByColor(expectedResult);
+            var sut = fixture.CreateSystemUnderTest();
+
+            // Act
+            var actionResult = sut.GetPersonsByColor("abc");
+
+            // Assert
+            var result = actionResult.Should()
+                .NotBeNull()
+                .And.BeOfType<BadRequestResult>()
+                ;
+        }
+    }
+
+    public class AddPerson : PersonsController
+    {
+        [Fact]
+        public  void ShouldReturnSavedObject_WhenRequestIsValid()
+        {
+            // Arrange
+            var expectedResult = fixture.Data.Person;
+            fixture.Mocks.SetupAddPerson(expectedResult);
+            var sut = fixture.CreateSystemUnderTest();
+
+            // Act
+            var actionResult = sut.AddPerson(expectedResult);
+
+            // Assert
+            var result = actionResult.Should()
+                .NotBeNull()
+                .And.BeOfType<CreatedAtRouteResult>()
+                .Subject
+                ;
+
+            var actualPersonResult = Assert.IsAssignableFrom<PersonDto>(result.Value);
+
+            actualPersonResult.Should()
+                .NotBeNull()
+                .And.BeEquivalentTo(expectedResult)
+                ;
+        }
+    }
+}
