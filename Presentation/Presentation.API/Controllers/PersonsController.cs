80e05da6381486f1d6d8188fcb48e94960118aba
behrouznd <behrouz1986@gmail.com>
2024-05-26 20:03:51 +0200

    modify person Controller

--- Presentation/Presentation.API/Controllers/PersonsController.cs
+++ Presentation/Presentation.API/Controllers/PersonsController.cs
@@ -30,11 +30,14 @@ public class PersonsController : ControllerBase
     /// Find person by Id
     /// </summary>
     /// <param name="id"></param>
-    /// <returns>The person</returns>
+    /// <returns code="200">The person</returns>
+	/// <response code="404">If id does not exist</response>
     [HttpGet("{id}" , Name = "PersonById")]
     public IActionResult GetPerson(int id) 
      { 
         var person = _personService.GetPersonById(id);
+        if (person is null)
+            return NotFound();
         return Ok(person);
     }
 
@@ -51,7 +54,7 @@ public class PersonsController : ControllerBase
     [ProducesResponseType(400)]
     public IActionResult GetPersonsByColor(string? color)
     {
-        if(Enum.TryParse<Colour>(color, out Colour colour))
+        if(Enum.TryParse<Colour>(color, out Colour colour) && !Int32.TryParse(color , out int color_))
         {
             var person = _personService.GetPeopleByColor((int)colour);
             return Ok(person);
